8404912401a27457cc307b4989cd52d1f8314409
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-04-30 21:39:31 +0630

    finished project setup and run

---
+++ webui-tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-2xl font-semibold text-center'>Hello, Responsive Website UI with Tailwind CSS</h1>
+    </div>
+  )
+}
+
+export default App
